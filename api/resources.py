592e9dff6d6dd61e8e494f057b705d0f120430f4
Alex Little <alex@alexlittle.net>
2013-02-14 13:12:09 +0100

    put override urls back - preprend_urls not working....

--- api/resources.py
+++ api/resources.py
@@ -70,7 +70,7 @@ class ModuleResource(ModelResource):
         always_return_data = True
         include_resource_uri = True
         
-    def prepend_urls(self):
+    def override_urls(self):
         return [
             url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/download%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('download_detail'), name="api_download_detail"),
             ]
