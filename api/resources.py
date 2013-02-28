c141e66536b7110816d49b13cb04873951f53b74
Alex Little <alex@alexlittle.net>
2013-02-28 13:12:56 +0100

    edit

--- api/resources.py
+++ api/resources.py
@@ -50,7 +50,7 @@ class TrackerResource(ModelResource):
         bundle.obj.agent = bundle.request.META.get('HTTP_USER_AGENT','unknown')
         # find out the module & activity type from the digest
         try:
-            bundle.obj.type = "page/quiz"
+            bundle.obj.type = bundle.data['digest']
             activity = Activity.objects.get(digest=bundle.data['digest'])
             bundle.obj.module = activity.section.module
             bundle.obj.type = activity.type
@@ -58,7 +58,6 @@ class TrackerResource(ModelResource):
             pass
         
         try:
-            bundle.obj.type = "maybemedia"
             media = Media.objects.get(digest=bundle.data['digest'])
             bundle.obj.module = media.module
             bundle.obj.type = 'media'
