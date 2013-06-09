c24244b08b0f2ce509318f2a87c73dba9ebdf3f0
alexlittle <alex@alexlittle.net>
2013-06-09 14:42:56 +0200

    updates for django 1.5

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 # learning_modules/api/serializers.py
-from django.core.serializers import json
-from django.utils import simplejson
+import json
+
 from tastypie.serializers import Serializer
 
 class PrettyJSONSerializer(Serializer):
@@ -9,7 +9,7 @@ class PrettyJSONSerializer(Serializer):
     def to_json(self, data, options=None):
         options = options or {}
         data = self.to_simple(data, options)
-        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
+        return json.dumps(data,
                 sort_keys=True, ensure_ascii=False, indent=self.json_indent)
      
 class ModuleJSONSerializer(Serializer):
@@ -22,8 +22,7 @@ class ModuleJSONSerializer(Serializer):
             data['modules'] = data['objects']
             del data['objects']
 
-        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
-               sort_keys=True)
+        return json.dumps(data,  sort_keys=True)
 
 class TagJSONSerializer(Serializer):
     json_indent = 4
@@ -43,5 +42,4 @@ class TagJSONSerializer(Serializer):
                 new_modules.append(m['module'])
             del data['modules']
             data['modules'] = new_modules
-        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
-               sort_keys=True, ensure_ascii=False)
+        return json.dumps(data, sort_keys=True, ensure_ascii=False)
