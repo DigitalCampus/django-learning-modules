73497c2121f07e412ae787a78c3e396706c806dd
alexlittle <alex@alexlittle.net>
2013-06-09 12:51:15 +0200

    remove deprecated function

--- api/resources.py
+++ api/resources.py
@@ -139,7 +139,7 @@ class ModuleResource(ModelResource):
         always_return_data = True
         include_resource_uri = True
         
-    def override_urls(self):
+    def prepend_urls(self):
         return [
             url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/download%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('download_detail'), name="api_download_detail"),
             ]
